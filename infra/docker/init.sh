c2ce6287c0ffd21e2050b858ab2a933382850c2b
Maulana Ibrahim <maulanaibrahim099@gmail.com>
2024-10-31 17:02:04 +0800

    Update readme.md

--- infra/docker/init.sh
+++ infra/docker/init.sh
@@ -3,9 +3,7 @@
 # create docker network
 docker network create main-network
 
-# Define the list of directories in the order to run docker-compose
-services=("04-postgresql")
-# services=("01-prometheus" "02-grafana" "03-elk-stack" "04-postgresql")
+services=("01-prometheus" "02-grafana" "03-elk-stack" "04-postgresql")
 
 # Loop through each service directory and run docker-compose up -d
 for service in "${services[@]}"; do
---
+++ infra/terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+# root/terraform.tfvars
+codename        = "hijra"
+project_id      = "test-project--01"
+project_region  = "asia-southeast2"
